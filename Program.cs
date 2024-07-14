321760ae6e6ce59debb4b678da3b964c68b9afd2
pchai <prempreychhay@gmail.com>
2024-07-14 07:57:30 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205Quiz1Part6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 5; // Width of each tree
+            int height = 3; // Height of each tree
+            int numTrees = 5; // Number of trees to repeat
+
+            // Loop for each tree
+            for (int tree = 0; tree < numTrees; tree++)
+            {
+                // Loop for each row of the tree
+                for (int i = height; i >= 1; i--)
+                {
+                    // Print spaces before asterisks
+                    for (int j = 0; j < height - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    // Print asterisks for the current row
+                    for (int k = 0; k < 2 * i - 1; k++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    // Move to the next line after each row
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
